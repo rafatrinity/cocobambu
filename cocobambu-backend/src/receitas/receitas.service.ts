808dd3f800a5848bfe1c70cb0dfd74e2834c07c0
Rafael Trindade <rafaeltrindade649@gmail.com>
2021-06-16 16:39:17 -0300

    add procfile

--- cocobambu-backend/src/receitas/receitas.service.ts
+++ cocobambu-backend/src/receitas/receitas.service.ts
@@ -1,32 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { CreateReceitaDto } from './dto/create-receita.dto';
-import { UpdateReceitaDto } from './dto/update-receita.dto';
-import { Receita, ReceitaDocument } from './entities/receita.entity';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { CreateReceitaDto } from "./dto/create-receita.dto";
+import { UpdateReceitaDto } from "./dto/update-receita.dto";
+import { Receita, ReceitaDocument } from "./entities/receita.entity";
 
 @Injectable()
 export class ReceitasService {
-  constructor(@InjectModel(Receita.name) private receitaModel: Model<ReceitaDocument>){}
-  
+  constructor(
+    @InjectModel(Receita.name) private receitaModel: Model<ReceitaDocument>
+  ) {}
+
   create(createReceitaDto: CreateReceitaDto) {
     const receita = new this.receitaModel(createReceitaDto);
     return receita.save();
   }
 
-  findAll(): Promise<Receita[]> {    
+  findAll(): Promise<Receita[]> {
     return this.receitaModel.find().exec();
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} receita`;
+    return this.receitaModel.findById(id);
   }
-
+  
   update(id: string, updateReceitaDto: UpdateReceitaDto) {
-    return `This action updates a #${id} receita`;
+    return this.receitaModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: updateReceitaDto },
+      { new: true }
+    );
   }
 
   remove(id: string) {
-    return `This action removes a #${id} receita`;
+    return this.receitaModel.deleteOne({_id:id})
   }
 }
